246f7044a1c5248f4f279e6e7197a698bd57194a
Daniel <71588528+ErDanieh@users.noreply.github.com>
2023-01-24 18:56:36 +0100

    ipfs

---
+++ nft-ipfs/my-app/pages/index.js
@@ -0,0 +1,150 @@
+import { Contract, providers, utils } from "ethers";
+import Head from "next/head";
+import React, { useEffect, useRef, useState } from "react";
+import Web3Modal from "web3modal";
+import { abi, NFT_CONTRACT_ADDRESS } from "../constants";
+import styles from "../styles/Home.module.css";
+
+export default function Home() {
+  const [walletConnected, setWalletConnected] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const [tokenIdsMinted, setTokenIdsMinted] = useState("0");
+  const web3ModalRef = useRef();
+
+  const getProviderOrSigner = async (needSigner = false) => {
+    const provider = await web3ModalRef.current.connect();
+    const web3Provider = new providers.Web3Provider(provider);
+
+    const { chainId } = await web3Provider.getNetwork();
+
+    if (chainId !== 80001) {
+      throw new Error("Please connect to Rinkeby Network");
+    }
+    if (needSigner) {
+      const signer = web3Provider.getSigner();
+      return signer;
+    }
+    return web3Provider;
+  };
+
+  const publicMint = async () => {
+    try {
+      console.log("Public Mint");
+
+      const signer = await getProviderOrSigner(true);
+      const nftContract = new Contract(NFT_CONTRACT_ADDRESS, abi, signer);
+      const tx = await nftContract.mint({
+        value: utils.parseEther("0.01"),
+      });
+
+      setLoading(true);
+      await tx.wait();
+      setLoading(false);
+      window.alert("Minted Successfully");
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const connectWallet = async () => {
+    try {
+      // Get the provider from web3Modal, which in our case is MetaMask
+      // When used for the first time, it prompts the user to connect their wallet
+      await getProviderOrSigner();
+      setWalletConnected(true);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  const getTokenIdsMinted = async () => {
+    try {
+      // Get the provider from web3Modal, which in our case is MetaMask
+      // No need for the Signer here, as we are only reading state from the blockchain
+      const provider = await getProviderOrSigner();
+      // We connect to the Contract using a Provider, so we will only
+      // have read-only access to the Contract
+      const nftContract = new Contract(NFT_CONTRACT_ADDRESS, abi, provider);
+      // call the tokenIds from the contract
+      const _tokenIds = await nftContract.tokenIds();
+      console.log("tokenIds", _tokenIds);
+      //_tokenIds is a `Big Number`. We need to convert the Big Number to a string
+      setTokenIdsMinted(_tokenIds.toString());
+    } catch (err) {
+      console.error(err);
+    }
+  };
+  useEffect(() => {
+    // if wallet is not connected, create a new instance of Web3Modal and connect the MetaMask wallet
+    if (!walletConnected) {
+      // Assign the Web3Modal class to the reference object by setting it's `current` value
+      // The `current` value is persisted throughout as long as this page is open
+      web3ModalRef.current = new Web3Modal({
+        network: "mumbai",
+        providerOptions: {},
+        disableInjectedProvider: false,
+      });
+
+      connectWallet();
+
+      getTokenIdsMinted();
+
+      // set an interval to get the number of token Ids minted every 5 seconds
+      setInterval(async function () {
+        await getTokenIdsMinted();
+      }, 5 * 1000);
+    }
+  }, [walletConnected]);
+
+  /*
+        renderButton: Returns a button based on the state of the dapp
+      */
+  const renderButton = () => {
+    // If wallet is not connected, return a button which allows them to connect their wallet
+    if (!walletConnected) {
+      return (
+        <button onClick={connectWallet} className={styles.button}>
+          Connect your wallet
+        </button>
+      );
+    }
+
+    // If we are currently waiting for something, return a loading button
+    if (loading) {
+      return <button className={styles.button}>Loading...</button>;
+    }
+
+    return (
+      <button className={styles.button} onClick={publicMint}>
+        Public Mint 🚀
+      </button>
+    );
+  };
+
+  return (
+    <div>
+      <Head>
+        <title>LW3Punks</title>
+        <meta name="description" content="LW3Punks-Dapp" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <div className={styles.main}>
+        <div>
+          <h1 className={styles.title}>Welcome to LW3Punks!</h1>
+          <div className={styles.description}>
+            Its an NFT collection for LearnWeb3 students.
+          </div>
+          <div className={styles.description}>
+            {tokenIdsMinted}/10 have been minted
+          </div>
+          {renderButton()}
+        </div>
+        <div>
+          <img className={styles.image} src="./LW3punks/1.png" />
+        </div>
+      </div>
+
+      <footer className={styles.footer}>Made with &#10084; by LW3Punks</footer>
+    </div>
+  );
+}
